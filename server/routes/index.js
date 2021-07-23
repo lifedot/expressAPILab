fc558920e0e8686bcb79b8604ed46268a5691bf9
Donald Smith <donaldsmith1911@gmail.com>
2021-07-22 22:07:44 -0500

    initial commit

---
+++ server/routes/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const apiRouter = require('./routes');
+const app = express();
+
+app.use(express.json());
---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const apiRouter = require('.routes');
+const app = express();
+
+app.use(express.json());
+
+app.use('/api', apiRouter)
